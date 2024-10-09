d57a7b1f56705668348f34ab51432f013abed38e
= <=>
2024-10-09 22:03:20 +0300

    PrimaryPage

---
+++ Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Security.Cryptography.X509Certificates;
+namespace OrganisationConferention
+{
+    public class Context:DbContext
+    {
+        public DbSet<PlanEvent> PlanEvent { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<ShortInformation> ShortInformation { get; set; }
+        private static Context _context;
+        public Context() : base("name=FedorConferentionEntities")
+        {
+
+        }
+        public static Context GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new Context();
+            }
+            return _context;
+        }
+    }
+}
---
+++ PrimaryPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrganisationConferention
+{
+    /// <summary>
+    /// Логика взаимодействия для PrimaryPage.xaml
+    /// </summary>
+    public partial class PrimaryPage : Page
+    {
+        public PrimaryPage()
+        {
+            InitializeComponent();
+            var currentTasks = Context.GetContext().ShortInformation.ToList();
+            LViewEvents.ItemsSource = currentTasks;
+            var allTypes = Context.GetContext().PlanEvent.ToList();
+            allTypes.Insert(0, new PlanEvent { Event = "Все типы" });
+            ComboType.ItemsSource = allTypes;
+            ComboType.SelectedIndex = 0;
+        }
+    }
+}
